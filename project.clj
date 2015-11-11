c78f024c413ad626ce0c8c16f58e93551aa458b0
Karsten Schmidt <k@postspectacular.com>
2015-11-11 10:32:49 +0000

    import project.clj and day1.core skeleton

---
+++ project.clj
@@ -0,0 +1,46 @@
+(defproject thi.ng/ws-ldn-2 "0.1.0-SNAPSHOT"
+  :description   "thi.ng workshop #2"
+  :url           "http://thi.ng"
+  :license       {:name "Apache Software License 2.0"
+                  :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies  [[org.clojure/clojure "1.7.0"]
+                  [org.clojure/clojurescript "1.7.170"]
+                  [thi.ng/geom "0.0.908"]
+                  [thi.ng/fabric "0.0.376"]
+                  [cljsjs/leaflet "0.7.3-4"]
+                  [cljsjs/codemirror "5.7.0-3"]
+                  [reagent "0.5.1"]
+                  [cljs-log "0.2.2"]]
+
+  :plugins       [[lein-figwheel "0.5.0-SNAPSHOT"]
+                  [lein-cljsbuild "1.1.1"]
+                  [lein-environ "1.0.0"]]
+
+  :source-paths  ["src/clj" "src/cljs"]
+
+  :clean-targets ^{:protect false} ["target" "resources/public/js"]
+
+  :profiles      {:prod {:env {:log-level 4}}}
+
+  :cljsbuild     {:builds [{:id           "day1"
+                            :source-paths ["src/cljs" "dev-src/day1"]
+                            :compiler     {:main                 ws-ldn-2.day1.core
+                                           :optimizations        :none
+                                           :asset-path           "js/out"
+                                           :output-to            "resources/public/js/app.js"
+                                           :output-dir           "resources/public/js/day1"
+                                           :source-map           true
+                                           :source-map-timestamp true
+                                           :cache-analysis       true}}
+                           {:id           "day1-min"
+                            :source-paths ["src/cljs"]
+                            :compiler     {:main          ws-ldn-2.day1.core
+                                           :optimizations :advanced
+                                           :pretty-print  false
+                                           :asset-path    "js/day1"
+                                           :output-to     "resources/public/js/app.js"}}]}
+
+  :figwheel      {:http-server-root "public"         ;; default and assumes "resources"
+                  :server-port 3449                  ;; default
+                  :css-dirs ["resources/public/css"] ;; watch and update CSS
+                  })
---
+++ src/clj/ws_ldn_2/day1/core.clj
@@ -0,0 +1,1 @@
+(ns ws-ldn-2.day1.core)
